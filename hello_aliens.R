2e49a63c7f8843d03cce1c646b975a80bd4347b0
Jeffrey Ho <Jeffrey@Jeffreys-MBP-2.wv.cc.cmu.edu>
2017-12-09 17:33:14 -0500

    commit - final project question 20

---
+++ hello_aliens.R
@@ -0,0 +1,3 @@
+cat("Hello, aliens, welcome to Earth. Did you really think you were the only species in our galaxy? 
+If so, that's cute. Obviously our technological marvels on planet Earth are second to none, but 
+    for you to get to where you are now is imporessive. Surely, this will probably all end in nuclear warfare, so be prepared.")
